a534668af3063a532be1cbe01de5d41c0f47abb6
jpccarreon <jccarreon3@up.edu.ph>
2021-11-03 14:32:17 +0800

    Changed Regex of keywords with more than 1
    -> Added comparison, relational, contatenation and typecast keywords

--- Loco/src/frontend/TokenKind.java
+++ Loco/src/frontend/TokenKind.java
@@ -3,19 +3,45 @@ package frontend;
 public enum TokenKind {
 	haiToken("HAI", 1), byeToken("KTHXBYE", 1),
 
-	numbrToken("-?[0-9]+", 1), numbarToken("-?[0-9]+\\.[0-9]+", 1), yarnToken("(\".+?|\".+?\")", -1), troofToken("WIN|FAIL", 1), typeToken("(TROOF|NUMBR|NUMBAR|YARN)", 1),
+	numbrToken("-?[0-9]+", 1), numbarToken("-?[0-9]+\\.[0-9]+", 1), yarnToken("(\".+?|\"[^\"]+\")", -1), troofToken("WIN|FAIL", 1), typeToken("(TROOF|NUMBR|NUMBAR|YARN)", 1),
 
 	btwToken("BTW", 1), obtwToken("OBTW", 1), tldrToken("TLDR", 1),
 
-	ihasToken("I", 3), itzToken("ITZ", 1), rToken("R", 1),
+	ihasToken("I\sHAS\sA", 3), itzToken("ITZ", 1), rToken("^R$", 1), 
 
-	sumOpToken("SUM", 2), diffOpToken("DIFF", 2), divOpToken("QUOSHUNT", 2), mulOpToken("PRODUKT", 2), modOpToken("MOD", 2), minOpToken("SMALLR", 2), maxOpToken("BIGGR", 2),
-
-	anToken("AN", 1), printToken("VISIBLE", 1),
+	sumOpToken("SUM\sOF", 2), 
+	diffOpToken("DIFF\sOF", 2), 
+	divOpToken("QUOSHUNT\sOF", 2), 
+	mulOpToken("PRODUKT\sOF", 2), 
+	modOpToken("MOD\sOF", 2), 
+	minOpToken("SMALLR\sOF", 2), 
+	maxOpToken("BIGGR\sOF", 2),
+	
+	bothOpToken("BOTH\sOF", 2), 
+	eitherOpToken("EITHER\sOF", 2), 
+	wonOpToken("WON\sOF", 2), 
+	notOpToken("NOT", 1), 
+	anyOpToken("ANY\sOF", 2), 
+	allOpToken("ALL\sOF", 2),
+	
+	bothSameOpToken("BOTH\sSAEM", 2),
+	diffrntOpToken("DIFFRINT", 1),
+	
+	smooshToken("SMOOSH", 1),
+	
+	isNowToken("IS\sNOW\sA", 3),
+	maekToken("MAEK", 1),
+	aToken("^A$", 1),
+	
+	anToken("^AN$", 1), printToken("VISIBLE", 1),
 	
 	varIdToken("[a-zA-Z][a-zA-Z0-9_]*", 1), loopIdToken("(?<=(IM\sIN\sYR\s))[a-zA-Z][a-zA-Z0-9_]*", 1),
 
-	eolToken("\n", 1), eofToken("\0", 1), miscToken(".+", 1);
+	quoteToken("\"", 1), 
+	
+	eolToken("\n", 1), eofToken("\0", 1), 
+	
+	miscToken("[^\"\s]+", 1), badToken("", 1);
 
 	private final String regex;
 	private final int length;

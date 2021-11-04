55e26cd861e6eec137e813b6a699e400072f8917
jpccarreon <jccarreon3@up.edu.ph>
2021-11-04 10:46:27 +0800

    Refactor NodeMathOp to NodeOperation
    -> Allows the class to be reused for boolean operation

--- Loco/src/SyntaxNodes/NodeOperation.java
+++ Loco/src/SyntaxNodes/NodeOperation.java
@@ -2,15 +2,20 @@ package SyntaxNodes;
 
 import frontend.Token;
 
-public class NodeMathOp extends SyntaxNode{
+public class NodeOperation extends SyntaxNode{
 	Token operation;
 	SyntaxNode operand1, operand2;
 	
-	public NodeMathOp (Token mathOp, SyntaxNode op1, SyntaxNode op2) {
+	public NodeOperation (Token mathOp, SyntaxNode op1, SyntaxNode op2) {
 		this.operation = mathOp;
 		this.operand1 = op1;
 		this.operand2 = op2;
 		
+	}
+	
+	public NodeOperation (Token mathOp, SyntaxNode op1) {
+		this.operation = mathOp;
+		this.operand1 = op1;
 	}
 	
 	public void printChildren(int tab) {
@@ -18,13 +23,16 @@ public class NodeMathOp extends SyntaxNode{
 		System.out.println("{");
 		
 		printTab(tab);
-		System.out.println("MathOP: " + operation.getKind());
+		System.out.println("Operation: " + operation.getValue());
 		printTab(tab);
 		System.out.println("0: ");
 		operand1.printChildren(tab + 1);
-		printTab(tab);
-		System.out.println("1: ");
-		operand2.printChildren(tab + 1);
+		
+		if (operand2 != null) {
+			printTab(tab);
+			System.out.println("1: ");
+			operand2.printChildren(tab + 1);
+		}
 		
 		printTab(tab);
 		System.out.println("}");

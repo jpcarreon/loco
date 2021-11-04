4815ab5b57c97a6db49163b7fe4a25175e4ce102
jpccarreon <jccarreon3@up.edu.ph>
2021-11-04 13:19:40 +0800

    Parse concat & fixed some error handling
    -> Yarn tokens are now produced once the current() is a quoteToken
    -> Fixed possible crashes as a result of loops in:
    	* parseYarn()
    	* parseInfArOp()
    	* parseComment()
    -> Moved boolean methods to the bottom

--- Loco/src/frontend/Parser.java
+++ Loco/src/frontend/Parser.java
@@ -58,7 +58,7 @@ public class Parser {
 			return nextToken();
 		}
 		
-		diagnostics.add("Line "+ lineCounter + ": Unexpected Token <"+ current().getKind() 
+		diagnostics.add("Line "+ lineCounter + ": Unexpected <"+ current().getKind() 
 						+ "> expected <"+ kind + ">");
 		return new Token(kind, null, current().getPosition());
 	}
@@ -93,6 +93,173 @@ public class Parser {
 		root.printChildren(0);
 		
 		return root;
+	}
+	
+	private SyntaxNode parseStatement() {
+		SyntaxNode expression = parseExpression();
+		match(TokenKind.eolToken);
+		consumeEOL();
+		
+		while (current().getKind() != TokenKind.byeToken) {
+			expression = new NodeStatement(expression, parseStatement());
+		}
+		
+		return expression;
+	}
+	
+	private SyntaxNode parseExpression() {
+		if (isMathOp()) {
+			return new NodeExpression(parseMathOp(), lineCounter);
+			
+		} else if (isComment()) {
+			return new NodeExpression(parseComment(), lineCounter);
+			
+		} else if (isAssignment()) {
+			return new NodeExpression(parseAssignment(), lineCounter);
+			
+		} else if (isBoolOp()) {
+			return new NodeExpression(parseBoolOp(), lineCounter);
+			
+		} else if (isInfArOp()) {
+			SyntaxNode infAr = new NodeExpression(parseInfArOp(nextToken()), lineCounter);
+			match(TokenKind.mkayToken);
+			return infAr;
+			
+		} else if (isCmpOp()) {
+			return new NodeExpression(parseCmpOp(), lineCounter);
+			
+		} else if (isConcatenation()) {
+			return new NodeExpression(parseConcat(nextToken()), lineCounter);
+		}
+		
+		diagnostics.add("Line "+ lineCounter + ": Invalid Keyword");
+		return new NodeLiteral(nextToken());
+	}
+	
+	private SyntaxNode parseMathOp() {
+		Token operation = nextToken();
+		
+		SyntaxNode operand1 = parseLiteral();
+		match(TokenKind.anToken);
+		SyntaxNode operand2 = parseLiteral();
+		
+		return new NodeOperation(operation, operand1, operand2);
+		
+	}
+	
+	private SyntaxNode parseComment() {
+		ArrayList<Token> inner = new ArrayList<Token>();
+		Token start = nextToken();
+		
+		while (current().getKind() != TokenKind.tldrToken && current().getKind() != TokenKind.byeToken) {
+			if (current().getValue() == "\n") lineCounter++;
+			inner.add(nextToken());
+		}
+		Token end = match(TokenKind.tldrToken);
+		
+		return new NodeComment(start, inner, end);
+	}
+	
+	private SyntaxNode parseAssignment() {
+		Token operation = nextToken();
+		Token varid = match(TokenKind.idToken);
+		
+		if (current().getKind() == TokenKind.itzToken) {
+			nextToken();
+			
+			if (isLiteral()) {
+				return new NodeAssignment(operation, varid, parseLiteral());
+			} else if (isMathOp()) {
+				return new NodeAssignment(operation, varid, parseMathOp());
+			} else if (current().getKind() == TokenKind.idToken) {
+				if (current().getValue() != varid.getValue()) {
+					//	TODO symbol table for variables
+					return new NodeAssignment(operation, varid);
+				}
+			}
+			
+			diagnostics.add("Line "+ lineCounter + ": Invalid assignment; expected valid Literal/VarId/Expression");
+			
+		}
+		
+		return new NodeAssignment(operation, varid);
+
+	}
+	
+	private SyntaxNode parseBoolOp() {
+		Token operation = nextToken();
+		
+		if (operation.getKind() != TokenKind.notOpToken) {
+			SyntaxNode operand1 = parseLiteral();
+			match(TokenKind.anToken);
+			SyntaxNode operand2 = parseLiteral();
+			
+			return new NodeOperation(operation, operand1, operand2);
+		} else {
+			SyntaxNode operand1 = parseLiteral();
+			
+			return new NodeOperation(operation, operand1);
+		}
+	}
+	
+	private SyntaxNode parseInfArOp(Token operation) {
+		SyntaxNode operand1 = parseLiteral();
+		
+		while (current().getKind() != TokenKind.mkayToken && current().getKind() != TokenKind.eolToken) {
+			match(TokenKind.anToken);
+			operand1 = new NodeOperation(operation, operand1, parseInfArOp(operation));
+		}
+		
+		return operand1;
+	}
+	
+	private SyntaxNode parseCmpOp() {
+		Token operation = nextToken();
+		
+		SyntaxNode operand1 = parseLiteral();
+		match(TokenKind.anToken);
+		SyntaxNode operand2 = parseLiteral();
+		
+		return new NodeOperation(operation, operand1, operand2);
+	}
+	
+	private SyntaxNode parseConcat(Token operation) {
+		SyntaxNode operand1 = parseLiteral();
+		
+		while (current().getKind() != TokenKind.eolToken) {
+			match(TokenKind.anToken);
+			operand1 = new NodeOperation(operation, operand1, parseConcat(operation));
+		}
+		
+		return operand1;
+
+	}
+	
+	private SyntaxNode parseLiteral() {
+		if (isMathOp()) {
+			return parseMathOp();
+		} else if (isBoolOp()) {
+			return parseBoolOp();
+		} else if (current().getKind() == TokenKind.quoteToken) {
+			return new NodeLiteral(parseYarn());
+		} else if (isLiteral()) {
+			return new NodeLiteral(nextToken());
+		} 
+		
+		return new NodeLiteral(match(TokenKind.numbrToken));
+	}
+	
+	private Token parseYarn() {
+		String value = new String();
+		nextToken();
+		
+		do {
+			value = value + nextToken().getValue() + " ";
+		} while (current().getKind() != TokenKind.quoteToken && current().getKind() != TokenKind.eolToken);
+		
+		match(TokenKind.quoteToken);
+		
+		return new Token(TokenKind.yarnToken, value, current().getPosition());
 	}
 	
 	private boolean isMathOp() {
@@ -149,7 +316,7 @@ public class Parser {
 	private boolean isLiteral() {
 		if (current().getKind() == TokenKind.numbrToken ||
 			current().getKind() == TokenKind.numbarToken ||
-			current().getKind() == TokenKind.yarnToken ||
+			current().getKind() == TokenKind.quoteToken ||
 			current().getKind() == TokenKind.troofToken ) {
 			return true;
 		}
@@ -164,143 +331,19 @@ public class Parser {
 		return false;
 	}
 	
+	private boolean isConcatenation() {
+		if (current().getKind() == TokenKind.smooshToken) {
+			return true;
+		}
+		
+		return false;
+	}
+
 	private void consumeEOL() {
 		while (current().getKind() == TokenKind.eolToken) {
 			lineCounter++;
 			nextToken();
 		}
 	}
-	
-	private SyntaxNode parseStatement() {
-		SyntaxNode expression = parseExpression();
-		match(TokenKind.eolToken);
-		
-		while (current().getKind() != TokenKind.byeToken) {
-			expression = new NodeStatement(expression, parseStatement());
-		}
-		
-		return expression;
-	}
-	
-	private SyntaxNode parseExpression() {
-		if (isMathOp()) {
-			return new NodeExpression(parseMathOp(), lineCounter);
-		} else if (isComment()) {
-			return new NodeExpression(parseComment(), lineCounter);
-		} else if (isAssignment()) {
-			return new NodeExpression(parseAssignment(), lineCounter);
-		} else if (isBoolOp()) {
-			return new NodeExpression(parseBoolOp(), lineCounter);
-		} else if (isInfArOp()) {
-			SyntaxNode infAr = new NodeExpression(parseInfArOp(nextToken()), lineCounter);
-			match(TokenKind.mkayToken);
-			return infAr;
-		} else if (isCmpOp()) {
-			return new NodeExpression(parseCmpOp(), lineCounter);
-		}
-		
-		diagnostics.add("Line "+ lineCounter + ": Invalid Keyword");
-		return new NodeLiteral(nextToken());
-	}
-	
-	private SyntaxNode parseMathOp() {
-		Token operation = nextToken();
-		
-		SyntaxNode operand1 = parseLiteral();
-		match(TokenKind.anToken);
-		SyntaxNode operand2 = parseLiteral();
-		
-		return new NodeOperation(operation, operand1, operand2);
-		
-	}
-	
-	private SyntaxNode parseComment() {
-		ArrayList<Token> inner = new ArrayList<Token>();
-		Token start = nextToken();
-		
-		while (current().getKind() != TokenKind.tldrToken) {
-			
-			inner.add(nextToken());
-		}
-		Token end = match(TokenKind.tldrToken);
-		
-		return new NodeComment(start, inner, end);
-	}
-	
-	private SyntaxNode parseAssignment() {
-		Token operation = nextToken();
-		Token varid = match(TokenKind.idToken);
-		
-		if (current().getKind() == TokenKind.itzToken) {
-			nextToken();
-			
-			if (isLiteral()) {
-				return new NodeAssignment(operation, varid, parseLiteral());
-			} else if (isMathOp()) {
-				return new NodeAssignment(operation, varid, parseMathOp());
-			} else if (current().getKind() == TokenKind.idToken) {
-				if (current().getValue() != varid.getValue()) {
-					//	TODO symbol table for variables
-					return new NodeAssignment(operation, varid);
-				}
-			}
-			
-			diagnostics.add("Line "+ lineCounter + ": Invalid assignment; expected valid Literal/VarId/Expression");
-			
-		}
-		
-		return new NodeAssignment(operation, varid);
-
-	}
-	
-	private SyntaxNode parseBoolOp() {
-		Token operation = nextToken();
-		
-		if (operation.getKind() != TokenKind.notOpToken) {
-			SyntaxNode operand1 = parseLiteral();
-			match(TokenKind.anToken);
-			SyntaxNode operand2 = parseLiteral();
-			
-			return new NodeOperation(operation, operand1, operand2);
-		} else {
-			SyntaxNode operand1 = parseLiteral();
-			
-			return new NodeOperation(operation, operand1);
-		}
-	}
-	
-	private SyntaxNode parseInfArOp(Token operation) {
-		SyntaxNode operand1 = parseLiteral();
-		
-		while (current().getKind() != TokenKind.mkayToken) {
-			match(TokenKind.anToken);
-			operand1 = new NodeOperation(operation, operand1, parseInfArOp(operation));
-		}
-		
-		return operand1;
-	}
-	
-	private SyntaxNode parseCmpOp() {
-		Token operation = nextToken();
-		
-		SyntaxNode operand1 = parseLiteral();
-		match(TokenKind.anToken);
-		SyntaxNode operand2 = parseLiteral();
-		
-		return new NodeOperation(operation, operand1, operand2);
-	}
-	
-	private SyntaxNode parseLiteral() {
-		if (isMathOp()) {
-			return parseMathOp();
-		} else if (isBoolOp()) {
-			return parseBoolOp();
-		} else if (isLiteral()) {
-			return new NodeLiteral(nextToken());
-		}
-		
-		return new NodeLiteral(match(TokenKind.numbrToken));
-	}	
-	
 	
 }

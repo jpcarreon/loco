16a9e7bc252a4bf1a512e04bc8464becb43bca97
jpccarreon <jccarreon3@up.edu.ph>
2021-11-24 09:03:05 +0800

    Refactoring NodeFunctionCall

--- Loco/src/main/java/frontend/Main.java
+++ Loco/src/main/java/frontend/Main.java
@@ -17,7 +17,8 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
 	public static void main(String[] args) {
-		startEvaluator();
+		//	TODO SHOW STATEMENT Node in the release version!
+		//startEvaluator();
 		//startLexer();
 		launch(args);
 	}

6584609e36e5c03dcaab927f4238870b9085665f
jpccarreon <jccarreon3@up.edu.ph>
2021-11-29 20:07:38 +0800

    Updated CodeHighlighter to correctly color syntax
    -> Changed colors:
        * Expressions are Grey
        * Assignments and Start/End are Red
        * Flow Control is Purple
        * I/O is Blue

--- Loco/src/main/java/frontend/CodeHighlighter.java
+++ Loco/src/main/java/frontend/CodeHighlighter.java
@@ -34,12 +34,21 @@ public class CodeHighlighter {
             "   BOTH OF var1 AN FAIL",
             "   EITHER OF \"2\" AN NOT 2.6",
             "   ALL OF 1 AN 2 AN 3 MKAY",
+            "",
+            "   O RLY?",
+            "      YA RLY",
+            "         VISIBLE \"TRUE\"",
+            "      MEBBE BOTH SAEM var1 AN 1",
+            "         VISIBLE \"var1 == 1\"",
+            "      NO WAI",
+            "         VISIBLE \"FALSE\"",
+            "   OIC",
             "KTHXBYE"
     });
 
     CodeHighlighter() {
         codeArea = new CodeArea();
-        codeArea.setLineHighlighterFill(Paint.valueOf("#E8E8FF"));
+        codeArea.setLineHighlighterFill(Paint.valueOf("#FFFFE4"));
         codeArea.setLineHighlighterOn(true);
         codeArea.setParagraphGraphicFactory(LineNumberFactory.get(codeArea));
 
@@ -50,8 +59,11 @@ public class CodeHighlighter {
         keywordSetup();
         allPattern = Pattern.compile(
                 "(?<EXPRESSION>" + expressionPattern + ")"
+                        + "|(?<ASSIGNMENT>" + assignmentPattern + ")"
+                        + "|(?<FLOWCONTROL>" + flowcontrolPattern + "|(\\?))"
+                        + "|(?<MISC>((\".+?\")))"
                         + "|(?<COMMENT>((OBTW.+?TLDR)|(BTW[^\n]*)))"
-                        + "|(?<SE>(HAI|KTHXBYE))",
+                        + "|(?<IO>(VISIBLE|GIMMEH|!))",
                 Pattern.DOTALL
         );
 
@@ -65,27 +77,46 @@ public class CodeHighlighter {
 
     private void keywordSetup() {
         String regex;
-        expressionPattern = new String();
+        expressionPattern = "\\b(";
+        assignmentPattern = "\\b(";
+        flowcontrolPattern = "\\b(";
 
-        expressionPattern += "\\b(";
+
         for (TokenKind i : TokenKind.values()) {
             regex = i.getRegex();
             regex = regex.replaceAll("\s", "\\\\s");
+            regex = regex.replaceAll("\\?", "\\\\?");
             regex = regex.replaceAll("(\\^|\\$)", "");
 
 
             if (i.getType().equals("mathop") || i.getType().equals("boolop") || i.getType().equals("cmpop") ||
-                    i.getType().equals("infarop") || i.getType().equals("concat") || i.getType().equals("print") ||
-                    i.getType().equals("exptypecast") || i == TokenKind.mkayToken || i == TokenKind.anToken ||
-                    i == TokenKind.aToken
-            ){
+                    i.getType().equals("infarop") || i.getType().equals("concat") || i.getType().equals("exptypecast") ||
+                    i == TokenKind.anToken || i == TokenKind.aToken
+            ) {
                 expressionPattern += regex + "|";
+            } else
+            if (i.getType().equals("asntypecast") || i.getType().equals("newvar") ||
+                    i.getType().equals("progStart") || i.getType().equals("progEnd") ||
+                    i == TokenKind.rToken || i == TokenKind.itzToken || i == TokenKind.mkayToken
+            ) {
+                assignmentPattern += regex + "|";
+            } else
+            if (i.getType().equals("ifstartblock") || i.getType().equals("ifblock") ||
+                    i.getType().equals("switchstart") || i.getType().equals("switch") ||
+                    i.getType().equals("loop") || i.getType().equals("function") ||
+                    i == TokenKind.breakToken
+            ) {
+                flowcontrolPattern += regex + "|";
             }
         }
-        expressionPattern = expressionPattern.substring(0, expressionPattern.length() - 1);
-        expressionPattern += ")\\b";
 
-        System.out.println(expressionPattern);
+        expressionPattern = expressionPattern.substring(0, expressionPattern.length() - 1);
+        assignmentPattern = assignmentPattern.substring(0, assignmentPattern.length() - 1);
+        flowcontrolPattern = flowcontrolPattern.substring(0, flowcontrolPattern.length() - 1);
+
+        expressionPattern += ")\\b";
+        assignmentPattern += ")\\b";
+        flowcontrolPattern += ")\\b";
     }
 
     private StyleSpans<Collection<String>> computeHighlighting(String text) {
@@ -99,18 +130,16 @@ public class CodeHighlighter {
 
             if (matcher.group("EXPRESSION") != null) {
                 styleClass = "expression";
-            /*
             } else if (matcher.group("ASSIGNMENT") != null) {
                 styleClass = "assignment";
             } else if (matcher.group("FLOWCONTROL") != null) {
                 styleClass = "flowcontrol";
-            } else if (matcher.group("LITERAL") != null) {
-                styleClass = "literal";
-            */
+            } else if (matcher.group("MISC") != null) {
+                styleClass = "misc";
             } else if (matcher.group("COMMENT") != null) {
                 styleClass = "comment";
-            } else if (matcher.group("SE") != null) {
-                styleClass = "se";
+            } else if (matcher.group("IO") != null) {
+                styleClass = "io";
             } else {
                 styleClass = null;
             }
